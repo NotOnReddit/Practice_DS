616a84bf4d50f41c6c5cad168ba0351c85e8220d
NotOnReddit <namloc.kn@gmail.com>
2023-05-23 12:14:07 -0700

    Add files via upload
    IBM DS assignment

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# 1. R
+# 2. Python
+# 3. C++
+# 4. SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. ggplot
+# 2. dplr
+# 3. pandas
+# 4. scikit-learn 
+
+# |Data Science Tools|
+# |-----|
+# |Python|
+# |Julia|
+# |Jupyter Notebook|
+# 
+
+# <h3>Below are a few examples of evaluating arithmetic expressions in Python 3</h3>
+# 
+# x=1
+# y=1
+# sum=x+y
+
+# In[2]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[4]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+minutes=200
+hours = minutes/60
+print(hours)
+
+
+# __Objectives:__
+# * Learn data science tools
+# * List popular languages for Data Science
+# * Create a table
+
+# <h2>Author: NK Nguyen <h2>
